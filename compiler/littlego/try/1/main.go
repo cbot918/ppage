2375044d79057f386a26c2d7ff5aa0161e17591d
cbot918 <cbot918@gmail.com>
2023-06-21 16:54:58 +0800

    update

--- compiler/littlego/try/1/main.go
+++ compiler/littlego/try/1/main.go
@@ -11,13 +11,26 @@ var Index int
 var Content []byte
 
 const (
-	path = "./code.txt"
+	defaultPath = "./print.txt"
 )
 
 const (
 	PRINT      = "print"
 	LEFTPAREN  = "("
 	RIGHTPAREN = ")"
+	DQUOTE     = "\""
+	PLUS       = "+"
+	MINUS      = "-"
+)
+
+const (
+	T_PRINT      = "PRINT"
+	T_LEFTPAREN  = "LEFTPAREN"
+	T_RIGHTPAREN = "RIGHTPAREN"
+	T_DQUOTE     = "DQUOTE"
+	T_NUMBER     = "NUMBER"
+	T_PLUS       = "PLUS"
+	T_MINUS      = "MINUS"
 )
 
 func GetContent(path string) []byte {
@@ -32,16 +45,36 @@ func isChar(char byte) bool {
 	return char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z'
 }
 
+func isNumber(char byte) bool {
+	return char >= '0' && char <= '9'
+}
+
 func GetOneString() string {
 	tempS := ""
 	for {
 		tempS += string(Content[Index])
 		Index += 1
-		if !isChar(Content[Index]) {
+		if Index >= len(Content) || !isChar(Content[Index]) {
 			break
 		}
 	}
 	return tempS
+}
+
+func GetOneNumber() string {
+	tempN := ""
+	for {
+		log("for")
+		tempN += string(Content[Index])
+		Index += 1
+		log("index in GetOneNumber: ", Index)
+		log("length of content: ", len(Content))
+
+		if Index >= len(Content) || !isNumber(Content[Index]) {
+			break
+		}
+	}
+	return tempN
 }
 
 func GetStringUntil(target string) string {
@@ -61,52 +94,90 @@ func GetStrings() []string {
 
 	codeLen := len(Content)
 
-	var tempA []string
-	for {
-
+	var strs []string
+	for Index < codeLen {
+		log("strs: ", strs)
+		// log(Index, string(Content[Index]))
 		// string a string
 		if isChar(Content[Index]) {
-			tempA = append(tempA, GetOneString())
+			strs = append(strs, GetOneString())
 			continue
 		}
 
-		// string a (
-		if string(Content[Index]) == "(" {
-			tempA = append(tempA, string(Content[Index]))
+		if isNumber(Content[Index]) {
+			strs = append(strs, GetOneNumber())
+			continue
 		}
-
-		// string a )
-		if string(Content[Index]) == ")" {
-			tempA = append(tempA, string(Content[Index]))
-		}
-
-		// deal with ""
-		if string(Content[Index]) == "\"" {
-			tempA = append(tempA, GetStringUntil("\""))
+		switch string(Content[Index]) {
+		case LEFTPAREN:
+			strs = append(strs, string(Content[Index]))
+		case RIGHTPAREN:
+			strs = append(strs, string(Content[Index]))
+		case PLUS:
+			log("+")
+			strs = append(strs, string(Content[Index]))
+		case MINUS:
+			strs = append(strs, string(Content[Index]))
+		case DQUOTE:
+			strs = append(strs, GetStringUntil("\""))
 		}
 
 		if Index < codeLen-1 {
 			Index++
 			continue
 		} else {
+
 			break
 		}
-
 	}
 
-	return tempA
+	return strs
+}
+
+func GetTokens(strings []string) []string {
+	tokens := []string{}
+	numberOrLiteral := "NUMBER"
+	for _, item := range strings {
+
+		if isChar(byte(item[0])) {
+			numberOrLiteral = "LITERAL"
+		}
+
+		switch item {
+		case LEFTPAREN:
+			tokens = append(tokens, T_LEFTPAREN)
+		case RIGHTPAREN:
+			tokens = append(tokens, T_RIGHTPAREN)
+		case PRINT:
+			tokens = append(tokens, T_PRINT)
+		case PLUS:
+			tokens = append(tokens, T_PLUS)
+		default:
+			tokens = append(tokens, numberOrLiteral)
+		}
+	}
+	return tokens
+}
+
+func GetFilePath() string {
+	args := os.Args
+	if len(args) == 2 {
+		log("input: ", args[1])
+		return "./" + args[1]
+	}
+	return defaultPath
 }
 
 func main() {
 	// init global var
 	Index = 0
-	Content = GetContent(path)
+	Content = GetContent(GetFilePath())
 
 	// content to strings
 	strings := GetStrings()
-	log(strings)
+	log("strings: ", strings)
 
-	// tokens := GetTokens(strings)
-
-	// Process(tokens)
+	tokens := GetTokens(strings)
+	log("tokens: ", tokens)
+	// Procestrs(tokens)
 }
